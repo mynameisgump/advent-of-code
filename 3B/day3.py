3311df406bac3e6fbdfd1ab2cd14cb5c3dca30ea
The Cryptic Canadian <ecrann@protonmail.com>
2019-12-05 12:44:13 -0330

    Finished day 3 part 2

--- 3B/day3.py
+++ 3B/day3.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         for section in wire:
             if section[0] == "R":
                 number = int(section[1:])
-                for i in range(x_val,x_val+number+1):
+                for i in range(x_val+1,x_val+number+1):
                     points.append([i,y_val])
                 x_val += number
             elif section[0] == "L":
@@ -24,7 +24,7 @@ if __name__ == "__main__":
                 x_val -= number
             elif section[0] == "U":
                 number = int(section[1:])
-                for i in range(y_val,y_val+number+1):
+                for i in range(y_val+1,y_val+number+1):
                     points.append([x_val,i])
                 y_val += number
             else:
@@ -34,13 +34,30 @@ if __name__ == "__main__":
                 y_val -= number
         all_points.append(points)
 
-    wire1_points = all_points[0]
-    wire2_points = all_points[1]
+    array1 = all_points[0]
+    #print("Wire 1")
+    #print(array1)
+    array2 = all_points[1]
+    #print("Wire 2")
+    #print(array2)
     final_points = []
-    intersections = [list(x) for x in set(tuple(x) for x in wire1_points).intersection(set(tuple(x) for x in wire2_points))]
-    min_dist = 100000000
+    intersections = [list(x) for x in set(tuple(x) for x in array1).intersection(set(tuple(x) for x in array2))]
+    #print("Intersections")
+    #print(intersections)
+    min_steps = 100000000
     for point in intersections:
-        dist = abs(point[0])+abs(point[1])
-        if ((dist < min_dist) and (dist != 0))  :
-            min_dist = dist
-    print(min_dist)
+        #print("Point")
+        #print(point)
+        #print("Steps: ")
+        #print(array1.index(point)+1)
+        #print(array2.index(point)+1)
+        steps = (array1.index(point)+1)+(array2.index(point)+1)
+        if ((steps < min_steps) and (steps != 2)):
+            min_steps = steps
+
+        #dist = abs(point[0])+abs(point[1])
+        #if ((dist < min_dist) and (dist != 0))  :
+        #    min_dist = dist
+
+    print("Minimum Steps")
+    print(min_steps)
