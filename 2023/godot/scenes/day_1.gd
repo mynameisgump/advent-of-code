2978b214cecf97d2494035f50d30c4e15598eb83
Ethan Crann <ecrann@protonmail.com>
2023-12-02 12:21:49 -0330

    Day 1 part 1 done in godot

--- 2023/godot/scenes/day_1.gd
+++ 2023/godot/scenes/day_1.gd
@@ -11,22 +11,23 @@ const files = {
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if run_day_1:
-		var file = FileAccess.open("res://data/01/example1.txt", FileAccess.READ);
+		# Part 1:
+		var file = FileAccess.open("res://data/01/example2.txt", FileAccess.READ);
 		var calibration_doc = file.get_as_text();
 		var lines = calibration_doc.split("\n");
-		print(lines)
 		var filtered_lines = []
+		var calibration_values = []
 		for line in lines:
 			var valid_digits = []
 			for char in line:
 				if char.is_valid_int():
 					valid_digits.append(char);
-			filtered_lines.append("".join(valid_digits))
-		print(filtered_lines[3][-1])
-		
+			var filtered_string = "".join(valid_digits);
 
-		
-	pass # Replace with function body.
+			calibration_values.append(int(filtered_string[0]+(filtered_string[-1])))
+		# Sum the Values
+		print(calibration_values.reduce(func(i, accum): return accum + i))
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
