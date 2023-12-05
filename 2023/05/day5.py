682741c29a15b4c7ea01fc0b59f619aaefcff36d
Ethan Crann <ecrann@protonmail.com>
2023-12-05 10:39:52 -0330

    Attempted part 1 but already hit a performance wall

--- 2023/05/day5.py
+++ 2023/05/day5.py
@@ -5,15 +5,34 @@ parser.add_argument("solution", choices=["p1","p2"])
 parser.add_argument("input", choices=["i1","ex1","ex2"])
 args = parser.parse_args();
 
+
+maps = []
+
 def part1(filename):
     with open(filename) as f:
         lines = f.read().split("\n\n");
-        print(lines)
+        print("All Lines: ",lines)
+        seeds_string = [int(num) for num in list(filter(None, lines[:1][0].split(":")[1].split(" ")))]
+        print("Seed string: ", seeds_string)
+        map_strings = [line.split("\n") for line in lines[1:]]
+        for plant_map in map_strings:
+            number_strings = plant_map[1:]
+            print("Maps strings: ",number_strings)
+            final_map = {}
+            for number_string in number_strings:
+                numbers = [int(num) for num in number_string.split(" ")]
+                [dest,source,length] = [int(num) for num in number_string.split(" ")]
+                dest_values = [*range(dest,dest+length)]
+                source_values = [*range(source,source+length)]
+                new_map = dict(map(lambda x, y: (x,y), source_values, dest_values))
+                print()
+                final_map.update(new_map)
+            print(final_map)
 
 def part2(filename):
     with open(filename) as f:
         lines = f.read().split("\n");
-
+       
 if __name__ == "__main__":
     input_selection = args.input
     solution_selection = args.solution;
