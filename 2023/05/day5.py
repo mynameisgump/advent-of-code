8074c8dc7cb585c44f00be3abc5b7ada2b46d557
Ethan Crann <ecrann@protonmail.com>
2023-12-05 11:02:40 -0330

    ezpz

--- 2023/05/day5.py
+++ 2023/05/day5.py
@@ -8,27 +8,56 @@ args = parser.parse_args();
 
 maps = []
 
+# Try storeing the range instead of the values 
+# AKA grab the 
+
+# Deal with jumping in the for loop
+# The create this for each line:
+# [source_lower,source_upper,length,dest]
+
+def calc_seed_value(seed_num,dest,source,length):
+    source_upper_bound = source+length;
+    if source <= seed_num < source_upper_bound:
+        dist_from_source = seed_num - source
+        final_dest = dest+dist_from_source
+        return final_dest
+    else:
+        return None
+
 def part1(filename):
     with open(filename) as f:
         lines = f.read().split("\n\n");
         print("All Lines: ",lines)
-        seeds_string = [int(num) for num in list(filter(None, lines[:1][0].split(":")[1].split(" ")))]
-        print("Seed string: ", seeds_string)
+        seeds = [int(num) for num in list(filter(None, lines[:1][0].split(":")[1].split(" ")))]
+        print("Seed string: ", seeds)
         map_strings = [line.split("\n") for line in lines[1:]]
         for plant_map in map_strings:
             number_strings = plant_map[1:]
             print("Maps strings: ",number_strings)
-            final_map = {}
+            mapping_values = []
             for number_string in number_strings:
                 numbers = [int(num) for num in number_string.split(" ")]
                 [dest,source,length] = [int(num) for num in number_string.split(" ")]
-                dest_values = [*range(dest,dest+length)]
-                source_values = [*range(source,source+length)]
-                new_map = dict(map(lambda x, y: (x,y), source_values, dest_values))
-                print()
-                final_map.update(new_map)
-            print(final_map)
-
+                source_upper_bound = source+length
+                mapping_values.append([dest,source,length])
+            print(mapping_values)
+            new_seeds = []
+            for seed_num in seeds:
+                final_seed_val = seed_num
+                for mapping_value in mapping_values:
+                    calculated_val = calc_seed_value(seed_num,mapping_value[0],mapping_value[1],mapping_value[2])
+                    if calculated_val:
+                        final_seed_val = calculated_val
+                print("Seed final value: ", final_seed_val)
+                new_seeds.append(final_seed_val)
+            seeds = new_seeds
+        print("Final seed: ", min(seeds))
+                # This does not work 
+                # dest_values = [*range(dest,dest+length)]
+                # source_values = [*range(source,source+length)]
+                # new_map = dict(map(lambda x, y: (x,y), source_values, dest_values))
+                # print()
+                # final_map.update(new_map)
 def part2(filename):
     with open(filename) as f:
         lines = f.read().split("\n");
