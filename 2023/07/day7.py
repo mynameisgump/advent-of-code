13a5464e15954b415f0ed2c03398dce3546801da
Ethan Crann <ecrann@protonmail.com>
2023-12-07 10:35:43 -0330

    Trying dif approach

--- 2023/07/day7.py
+++ 2023/07/day7.py
@@ -43,35 +43,23 @@ def judge_hand(hand):
     for card in hand:
         card_vals[card] = card_vals.get(card, 0) + 1
     
-
     if sorted(card_vals.values()) == [5]:
         return 6
-        #return ("five_kind")
     if sorted(card_vals.values()) == [1,4]:
         return 5
-        #return ("four_kind")
     if sorted(card_vals.values()) == [2,3]:
         return 4
-        #return ("full_house")
     if sorted(card_vals.values()) == [1,1,3]:
         return 3
-        #return ("three_kind")
     if sorted(card_vals.values()) == [1,2,2]:
         return 2 
-        #return ("two_pair")
     if sorted(card_vals.values()) == [1,1,1,2]:
         return 1 
-        #return ("one_pair")
     if sorted(card_vals.values()) == [1,1,1,1,1]:
         return 0
-        #return ("high_card")
-        
-    
-
     print(card_vals)
-    # five_kind = hand == len(hand) * hand[0]
-    # if five_kind:
-    #     return (five_kind,hand[0])
+
+
     
 def sort_hand(hand_1, hand_2):
     hand_1_type = judge_hand(hand_1)
@@ -90,11 +78,6 @@ def sort_hand(hand_1, hand_2):
                 return -1
         return 0
 
-
-# Do first pass to get all types of hands
-# Then determines ranking within these hands 
-# Or sort them all idaf
-
 def part1(filename):
     with open(filename) as f:
         lines = f.read().split("\n");
@@ -103,26 +86,116 @@ def part1(filename):
         players = dict(map(lambda i,j : (i,j) , hands,bids))
 
         sorted_winners = sorted(hands, key=cmp_to_key(sort_hand))
-        # cur_game = 0
-        # while cur_game < len(hands):
-        #     cur_hand = hands[cur_game]
-        #     cur_bid = hands[cur_game]
-            #fiveKind =
-            #rint(test)
-            # judged_hand = judge_hand(cur_hand)
-            # print(judged_hand)
-            # cur_game += 1
         final_sum = 0
         for i in range(len(sorted_winners)):
             bid = players[sorted_winners[i]] * (i+1)
             final_sum += bid
-            print("Bid:", bid)
-        print("Hands: ", hands)
-        print("Bids: ", bids)
         print("Sum: ", final_sum)
+
+def judge_hand_p2(hand):
+    card_vals = {}
+    for card in hand:
+        card_vals[card] = card_vals.get(card, 0) + 1
+    
+    # Five Kind
+    # Four Kind 
+    # Full house
+    # Three Kind
+    # Two Pair
+    # One Pair 
+    # High Card
+    if "J" not in card_vals.keys():
+        if sorted(card_vals.values()) == [5]:
+            return 6
+        if sorted(card_vals.values()) == [1,4]:
+            return 5
+        if sorted(card_vals.values()) == [2,3]:
+            return 4
+        if sorted(card_vals.values()) == [1,1,3]:
+            return 3
+        if sorted(card_vals.values()) == [1,2,2]:
+            return 2 
+        if sorted(card_vals.values()) == [1,1,1,2]:
+            return 1 
+        if sorted(card_vals.values()) == [1,1,1,1,1]:
+            return 0
+        print(card_vals)
+    else:
+        total_jokers = card_vals.pop("J")
+        max_val = max(card_vals, key=card_vals.get)
+        card_vals
+
+        # Five Kind
+        # if sorted(card_vals.values()) == [5]:
+        #     return 6
+        # if sorted(card_vals.values()) == [4]:
+        #     return 6
+        # if sorted(card_vals)
+        
+        # Four Kind
+
+
+
+        # print("Judging hand w/ J: ", hand)
+        # 
+        # print(sorted(card_vals.values()))
+        # 1 joker
+        # if sorted(card_vals.values()) == [4]:
+        #     return 6
+        # if sorted(card_vals.values()) == [1,3]:
+        #     return 5
+        # if sorted(card_vals.values()) == [2,2]:
+        #     return 4
+        # if sorted(card_vals.values()) == [1,1,2]:
+        #     return 3
+        # if sorted(card_vals.values()) == [1,1,1,1]:
+        #     return 1
+        # 2 joker
+        # if sorted(card_vals.values()) == [1,2]:
+        #     return 4
+        # if sorted(card_vals.values()) == [2,2]:
+        #     return 4
+        # if sorted(card_vals.values()) == [2,2]:
+        #     return 4
+        
+            
+
+
+        print(total_jokers)
+    return 0
+
+def sort_hand_p2(hand_1, hand_2):
+    hand_1_type = judge_hand_p2(hand_1)
+    hand_2_type = judge_hand_p2(hand_2)
+    if hand_1_type > hand_2_type:
+        return 1
+    elif hand_2_type > hand_1_type:
+        return -1
+    elif hand_1_type == hand_2_type:
+        for i in range(len(hand_1)):
+            char_1 = card_strength[hand_1[i]]
+            char_2 = card_strength[hand_2[i]]
+            if char_1 > char_2:
+                return 1
+            if char_2 > char_1: 
+                return -1
+        return 0
+
+
+
 def part2(filename):
     with open(filename) as f:
         lines = f.read().split("\n");
+        hands = [line.split(" ")[0] for line in lines]
+        bids = [int(line.split(" ")[1]) for line in lines]
+        players = dict(map(lambda i,j : (i,j) , hands,bids))
+
+        sorted_winners = sorted(hands, key=cmp_to_key(sort_hand_p2))
+        final_sum = 0
+        for i in range(len(sorted_winners)):
+            bid = players[sorted_winners[i]] * (i+1)
+            final_sum += bid
+        print("Sum: ", final_sum)
 
 if __name__ == "__main__":
     input_selection = args.input
