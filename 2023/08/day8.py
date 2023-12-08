5dacc9aa4b8ed038860bb6c28ca6d79403e5b306
Ethan Crann <ecrann@protonmail.com>
2023-12-08 10:46:45 -0330

    Ideas for part 2

--- 2023/08/day8.py
+++ 2023/08/day8.py
@@ -1,9 +1,44 @@
 import argparse
 import re
+from threading import Thread
+
 parser = argparse.ArgumentParser()
 parser.add_argument("solution", choices=["p1","p2"])
 parser.add_argument("input", choices=["i1","ex1","ex2"])
 args = parser.parse_args();
+
+# Have each thread updating a var with the parts it hit's Z,
+# Collapse when all intersect with a given step number
+# Steps: 
+# 1. Get all that end with A
+# 2. Create threads which wait for others to end when they hit a Z
+# 3. From there continue by passing back to the function the current Z steps 
+#    and the current key you were searching from
+
+def threaded_path(starting_key,instructions,path_map,total_z):
+    key = starting_key
+
+    found = False
+    steps = 0 
+    cur_index = 0
+    key = starting_key
+    z_steps = []
+    while not found and len(z_steps) < total_z:
+        if found:
+            found = False
+        if cur_index > len(instructions)-1:
+            cur_index = 0
+        char = instructions[cur_index]
+        if char == "L":
+            key = path_map[key][0]
+        elif char == "R":
+            key = path_map[key][1]
+        steps += 1
+        cur_index += 1
+        if key.endswith():
+            found = True
+    return z_steps
+
 
 def part1(filename):
     with open(filename) as f:
@@ -15,17 +50,11 @@ def part1(filename):
             pairs[i][1] = re.sub("[()]",'',pairs[i][1]).strip().split(", ")
             path_map[pairs[i][0]] = pairs[i][1]
         
-        print(path_map)
-
         found = False
         steps = 0 
         cur_index = 0
         key = "AAA"
         while not found:
-            print(key)
-            print(cur_index)
-            print(steps)
-            print(len(instructions))
             if cur_index > len(instructions)-1:
                 cur_index = 0
             char = instructions[cur_index]
@@ -37,16 +66,26 @@ def part1(filename):
             cur_index += 1
             if key == "ZZZ":
                 found = True
-            
         print(steps)
-
-
-            
-            
 
 def part2(filename):
     with open(filename) as f:
-        lines = f.read().split("\n");
+        [instructions, nodes] = f.read().split("\n\n");
+        pairs = [item.strip().split("=") for item in nodes.split("\n")]
+        path_map = {}
+        sum_A = 0
+        for i in range(len(pairs)):
+            pairs[i][0] = pairs[i][0].strip()
+            if pairs[i][0].endswith("A"):
+                print(pairs[i][0])
+                sum_A += 1
+            pairs[i][1] = re.sub("[()]",'',pairs[i][1]).strip().split(", ")
+            #print(pairs[i])
+
+            path_map[pairs[i][0]] = pairs[i][1]
+        print(sum_A)
+        #print(path_map)
+        for i in tange
 
 if __name__ == "__main__":
     input_selection = args.input
@@ -57,6 +96,8 @@ if __name__ == "__main__":
             filename="input.txt"
         case "ex1":
             filename="example1.txt"
+        case "ex2":
+            filename="example2.txt"
     match solution_selection:
         case "p1":
             part1(filename)
