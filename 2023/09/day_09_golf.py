94fff5d59667d4d7dad8cb01a69929b569c2de35
Ethan Crann <ecrann@protonmail.com>
2023-12-10 01:44:22 -0330

    Reduced answer size

--- 2023/09/day_09_golf.py
+++ 2023/09/day_09_golf.py
@@ -1,75 +1,33 @@
 import argparse
 import re
-parser = argparse.ArgumentParser()
-parser.add_argument("solution", choices=["p1","p2"])
-parser.add_argument("input", choices=["i1","ex1"])
-args = parser.parse_args();
-def part1(filename):
-    with open(filename) as f:
-        lines = [list(map(int, item.split(" "))) for item in f.read().split("\n")];
-        total_sum = 0
-        for line in lines:
-            done = False
-            current = line
-            sequences = [line+[0]]
-            while done == False:
-                diff = [j-i for i, j in zip(current[:-1], current[1:])]
-                sequences.append(diff+[0])
-                current = diff
-                if all(val == 0 for val in diff):
-                    done = True
-            sequences.reverse()
-            #print("Sequences:", sequences)
-            for i in range(len(sequences)-1):
-                seq = sequences[i]
-                seq_2 = sequences[i+1]
-                last_val_1 = seq[-1]
-                last_val_2 = seq_2[-2]
-                seq_2[-1] = last_val_2+last_val_1
-            #print("seq_result", sequences[-1][-1]) 
-            total_sum += sequences[-1][-1]
-        print("Total Sum: ",total_sum)
-        
+from functools import partial
 
-def part2(filename):
-    with open(filename) as f:
-        lines = [list(map(int, item.split(" "))) for item in f.read().split("\n")];
-        total_sum = 0
-        for line in lines:
-            line.reverse()
-            done = False
-            current = line
-            sequences = [line+[0]]
-            while done == False:
-                diff = [j-i for i, j in zip(current[:-1], current[1:])]
-                sequences.append(diff+[0])
-                current = diff
-                if all(val == 0 for val in diff):
-                    done = True
-            sequences.reverse()
-            #print("Sequences:", sequences)
-            for i in range(len(sequences)-1):
-                seq = sequences[i]
-                seq_2 = sequences[i+1]
-                last_val_1 = seq[-1]
-                last_val_2 = seq_2[-2]
-                seq_2[-1] = last_val_2+last_val_1
-            total_sum += sequences[-1][-1]
-        print("Total Sum: ",total_sum)
+def get_predicted(history, reverse):
+    done = False
+    if reverse:
+        history.reverse()
+    current = history
+    sequences = [history+[0]]
+    while done == False:
+        diff = [j-i for i, j in zip(current[:-1], current[1:])]
+        sequences.append(diff+[0])
+        current = diff
+        if all(val == 0 for val in diff):
+            done = True
+    sequences.reverse()
+    for i in range(len(sequences)-1):
+        seq = sequences[i]
+        seq_2 = sequences[i+1]
+        last_val_1 = seq[-1]
+        last_val_2 = seq_2[-2]
+        seq_2[-1] = last_val_2+last_val_1
+    return sequences[-1][-1]
 
 if __name__ == "__main__":
-    input_selection = args.input
-    solution_selection = args.solution;
-    filename = ""
-    match input_selection:
-        case "i1":
-            filename="input.txt"
-        case "ex1":
-            filename="example1.txt"
-    match solution_selection:
-        case "p1":
-            part1(filename)
-        case "p2":
-            part2(filename)
-
-def main
+    filename = "example1.txt"
+    with open(filename) as f:
+        histories = [list(map(int, item.split(" "))) for item in f.read().split("\n")];
+        part1 = sum(list(map(partial(get_predicted,reverse=False),histories)))
+        print("Part 1: ", part1)
+        part2 = sum(list(map(partial(get_predicted,reverse=True),histories))) 
+        print("Part 2: ", part2)
