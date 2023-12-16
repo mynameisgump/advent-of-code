e85d76b2d505090241a6f1bb0d97c875d3ffeef5
Ethan Crann <ecrann@protonmail.com>
2023-12-16 12:51:34 -0330

    Day 15 done

--- 2023/15/day15.py
+++ 2023/15/day15.py
@@ -1,5 +1,6 @@
 import argparse
-import re
+from collections import OrderedDict
+
 parser = argparse.ArgumentParser()
 parser.add_argument("solution", choices=["p1","p2"])
 parser.add_argument("input", choices=["i1","ex1","ex2"])
@@ -25,18 +26,30 @@ def part2(filename):
     with open(filename) as f:
         lines = f.read().split("\n")[0].split(",");
         total = 0 
-        hashmap = {k: {} for k in range(256)}
+        hashmap = {k: OrderedDict() for k in range(256)}
         for string in lines:
             if "=" in string:
                 [label, focal_length] = string.split("=")
                 print(label,focal_length)
                 box_num = hash_string(label)
-                hashmap[box_num][label] = focal_length
+                hashmap[box_num][label] = int(focal_length)
                 pass
             elif "-" in string:
-                pass
-            total += hash_string(string)
-        print(total)
+                label = string.replace("-","")
+                box_num = hash_string(label)
+                if label in hashmap[box_num]:
+                    del hashmap[box_num][label]
+        filtered_dict = {k: v for k, v in hashmap.items() if len(v) > 0}
+        print(filtered_dict)
+        values = []
+        for box_num, items in filtered_dict.items():
+            cur_index = 0 
+            for label,focal_length in items.items():
+                cur_index += 1
+                values.append((box_num+1)*cur_index*focal_length)
+        print(sum(values))
+            # total += (1+box_num)*
+            # print(box_num,items)
 
 if __name__ == "__main__":
     input_selection = args.input
