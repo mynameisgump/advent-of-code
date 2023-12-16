504358fa83d725df9774515195660eb8a8e71ee9
Ethan Crann <ecrann@protonmail.com>
2023-12-16 11:59:41 -0330

    day 13 part 1 done

--- 2023/13/day13.py
+++ 2023/13/day13.py
@@ -8,6 +8,7 @@ args = parser.parse_args();
 # First then second: sorted_list = sorted(list, key=lambda x: (x[0], -x[1]))
 # Attempt 1: 51329 :(
 # Attemp 2: 38687 :(((
+# Attempt 3: 35360 :)))
 # #.##.#.##.#.#
 # #.#.##.#.##.#
 def part1(filename):
@@ -28,41 +29,18 @@ def part1(filename):
                 columns.append(cur_column)
             rows = mirror.copy()
 
-            #print("Rows: ", rows)
-            #print("Columns: ", columns)
-            # mirrored_rows = []
-            # while len(rows) > 0:
-            #     front_ele = rows.pop(0)
-            #     mirrored_rows.insert(0,front_ele)
-            #     if len(rows) > 0:
-            #         print()
-            #         print(rows)
-            #         print(mirrored_rows)
-            #         print("Rows as subset", all(x in mirrored_rows for x in rows))
-            #         print("Mirrored as subset",all(x in rows for x in mirrored_rows))
-            
+ 
             col_index = 0
             mirrored_cols = []
             while len(columns) > 0:
                 front_ele = columns.pop(0)
                 mirrored_cols.insert(0,front_ele)
-                if len(columns) > 0:
-                    #print(all(x in columns for x in mirrored_cols))
-                    
+                if len(columns) > 0:                    
                     if all(x == y for x, y in zip(columns, mirrored_cols)):
                         col_index = len(mirrored_cols)
+                        total += col_index
                         break 
-                    
 
-                    # if (all(x in mirrored_cols for x in columns)):
-                    #     print()
-                    #     print(columns)
-                    #     print(mirrored_cols)
-                    #     print("Cols as subset", all(x in mirrored_cols for x in columns))
-                    #     print("Mirrored as subset",all(x in columns for x in mirrored_cols))
-                    #     [print(len(mirrored_cols))]
-                    #     col_index = len(mirrored_cols)
-                    #     break
             print("Col Index: ", col_index)
             row_index = 0
             mirrored_rows = []
@@ -70,50 +48,13 @@ def part1(filename):
                 front_ele = rows.pop(0)
                 mirrored_rows.insert(0,front_ele)
                 if len(rows) > 0:
-                    
-                    if (all(x in mirrored_rows for x in rows)):
+                    if all(x == y for x, y in zip(rows, mirrored_rows)):
                         row_index = len(mirrored_rows)
-                        break 
+                        total += row_index*100
+                        break
             print("Row Index: ",row_index)
-                        # print()
-                        # print(rows)
-                        # print(mirrored_rows)
-                        # print("Rows as subset", all(x in mirrored_rows for x in rows))
-                        # print("Mirrored Rows as subset",all(x in rows for x in mirrored_rows))
-                        # [print(len(mirrored_rows))]
-                
-                
-            #print(columns)
-            # row_hit = False
-            # r_duplicates = [item for item, count in collections.Counter(mirror).items() if count > 1]
-
-            # rows = mirror.copy()
-            # while not perfect:
-            #     count = collections.Counter(rows)
-            #     print(count)
-
-            # print(r_duplicates)
-            # collections.Counter()
-
-            # r_indices = [i for i in range(len(mirror)) if mirror[i] in r_duplicates]
-            # if len(r_duplicates) > 1:
-            #     if r_indices[-1] == len(mirror)-1 or r_indices[0] == 0:
-            #         print(r_duplicates,r_indices)
-            #         total += r_indices[len(r_indices)//2]*100
-            #         print("Row Split:", r_indices[len(r_indices)//2])
-
-            c_duplicates = [item for item, count in collections.Counter(columns).items() if count > 1]
-            #print("Counts:",collections.Counter(columns))
-            c_indices = [i for i in range(len(columns)) if columns[i] in c_duplicates]
-            
-            if len(c_duplicates) > 1:
-                if c_indices[-1] == len(columns)-1 or c_indices[0] == 0:
-                    #print(c_duplicates,c_indices)
-                    total += c_indices[len(c_indices)//2]
-                    #print("Column Split:", c_indices[len(c_indices)//2])
-
        
-
+            print("Total: ", total)
         #print(h_indices,v_indices)
         #print(total)
         #print(lines)
