d2ba1db24ec1c325e28362b3c24dd1db5662ced4
Ethan Crann <ecrann@protonmail.com>
2023-12-17 12:30:09 -0330

    gonna try caching

--- 2023/14/day14.py
+++ 2023/14/day14.py
@@ -1,17 +1,51 @@
 import argparse
 import re
 from dataclasses import dataclass
-
+from functools import cache
 parser = argparse.ArgumentParser()
 parser.add_argument("solution", choices=["p1","p2"])
 parser.add_argument("input", choices=["i1","ex1","ex2"])
 args = parser.parse_args();
 
+@cache
+def cycle(rows):
+    for i in range(4):
+        for col in range(len(rows[0])):
+            cur_collisions = []
+            hit_empty = False
+            for row in range(len(rows)):
+                char = rows[row][col]
+                match char:
+                    case "O":
+                        if not hit_empty:
+                            cur_collisions.append("#")
+                        else:        
+                            dist = len(cur_collisions)-1
+                            final_index = len(cur_collisions)-1
+                            while dist >= 0:
+                                comp_char = cur_collisions[dist]
+                                if comp_char == ".":
+                                    final_index = dist
+                                if comp_char == "#":
+                                    break
+                                dist -= 1
+                            rows[final_index][col] = "O"
+                            rows[row][col] = "."
+                            cur_collisions[final_index] = "#"
+                            cur_collisions.append(".")
+                    case ".":
+                        hit_empty = True
+                        cur_collisions.append(".")
+                    case "#":
+                        hit_empty = False
+                        cur_collisions.append("#")
+        rows = [list(row) for row in zip(*rows[::-1])]
+    return rows  
 
 def print_matrix(matrix):
     for row in matrix:
         print("".join(row))
-
+# 112773
 def part1(filename):
     with open(filename) as f:
         rows = [[*row] for row in f.read().split("\n")];
@@ -21,24 +55,16 @@ def part1(filename):
             for row in rows:
                 cur_column.append(row[i])
             columns.append(cur_column)
-        #print_matrix(rows)
-        #print()
-        #print_matrix(columns)
         for col in range(len(rows[0])):
-            #print()
-            #print("Processing Column: ", columns[col])
             cur_collisions = []
             hit_empty = False
             for row in range(len(rows)):
                 char = rows[row][col]
-                #print(char)
                 match char:
                     case "O":
                         if not hit_empty:
                             cur_collisions.append("#")
-                        else:
-                            #print("ZZZZZ")
-                            #print(cur_collisions)            
+                        else:        
                             empty = True 
                             dist = len(cur_collisions)-1
                             final_index = len(cur_collisions)-1
@@ -49,12 +75,8 @@ def part1(filename):
                                 if comp_char == "#":
                                     break
                                 dist -= 1
-                            #print("Final Index: ",final_index)
-                            #print_matrix(rows)
                             rows[final_index][col] = "O"
                             rows[row][col] = "."
-                            #print()
-                            #print_matrix(rows)
                             cur_collisions[final_index] = "#"
                             cur_collisions.append(".")
                     case ".":
@@ -63,7 +85,6 @@ def part1(filename):
                     case "#":
                         hit_empty = False
                         cur_collisions.append("#")
-                #print(cur_collisions)
         print("Rows: ")
         print_matrix(rows)
         total = 0
@@ -78,7 +99,61 @@ def part1(filename):
 
 def part2(filename):
     with open(filename) as f:
-        lines = f.read().split("\n");
+        rows = [[*row] for row in f.read().split("\n")];
+        # print_matrix(rows)
+        # rows = cycle(rows)
+        # print()
+        # print_matrix(rows)
+        for z in range(1000000000):
+            print(z)
+            rows = cycle(rows)
+        #     for i in range(4):
+        #         for col in range(len(rows[0])):
+        #             cur_collisions = []
+        #             hit_empty = False
+        #             for row in range(len(rows)):
+        #                 char = rows[row][col]
+        #                 match char:
+        #                     case "O":
+        #                         if not hit_empty:
+        #                             cur_collisions.append("#")
+        #                         else:        
+        #                             dist = len(cur_collisions)-1
+        #                             final_index = len(cur_collisions)-1
+        #                             while dist >= 0:
+        #                                 comp_char = cur_collisions[dist]
+        #                                 if comp_char == ".":
+        #                                     final_index = dist
+        #                                 if comp_char == "#":
+        #                                     break
+        #                                 dist -= 1
+        #                             rows[final_index][col] = "O"
+        #                             rows[row][col] = "."
+        #                             cur_collisions[final_index] = "#"
+        #                             cur_collisions.append(".")
+        #                     case ".":
+        #                         hit_empty = True
+        #                         cur_collisions.append(".")
+        #                     case "#":
+        #                         hit_empty = False
+        #                         cur_collisions.append("#")
+        #         # print("After turn:")
+        #         # print_matrix(rows)
+        #         # print() 98887 too low 
+        #         # 98898 
+        #         rows = [list(row) for row in zip(*rows[::-1])]
+            
+        #     total = 0
+        #     row_load = len(rows)
+        #     for row in rows:
+        #         total += row.count("O") * row_load
+        #         row_load -= 1
+        #     print(z, total)
+
+        #print_matrix(rows)98893
+        
+        #print(total)
+            
 
 if __name__ == "__main__":
     input_selection = args.input
