7a0c95ff3539ae95f871f172727bcb350566615e
Ethan Crann <ecrann@protonmail.com>
2023-12-18 09:00:05 -0330

    day 16 part 2 done

--- 2023/16/day16.py
+++ 2023/16/day16.py
@@ -17,16 +17,19 @@ def part1(filename):
     with open(filename) as f:
         laser_map = [[*row] for row in f.read().split("\n")];
         initial_char = laser_map[0][0];
-        lasers = [((0,0),"R")]
+        lasers = [((0,-1),"R")]
         print_matrix(laser_map)
         laser_map_dup =  [x[:] for x in laser_map]
         seen_lasers = set()
+        init_flag = False
         while len(lasers) > 0 :
             laser = lasers.pop()
             print(laser)
             cur_pos = laser[0]
             direction = laser[1]
-            laser_map_dup[cur_pos[0]][cur_pos[1]] = "#"
+            if init_flag:
+                laser_map_dup[cur_pos[0]][cur_pos[1]] = "#"
+            init_flag = True
             #char = laser_map[laser[1][0]][laser[1][1]]
             next_pos = ()
             match direction:
@@ -93,9 +96,313 @@ def part1(filename):
         map_string += "".join(row)
     print(map_string.count("#"))
     print_matrix(laser_map_dup)
+
+
 def part2(filename):
     with open(filename) as f:
-        lines = f.read().split("\n");
+        laser_map = [[*row] for row in f.read().split("\n")];
+        laser_map_dup =  [x[:] for x in laser_map]
+        totals = set()
+        # Left Column
+        for i in range(len(laser_map)):
+            laser_map_dup =  [x[:] for x in laser_map]
+            lasers = [((i,-1),"R")]
+            seen_lasers = set()
+            init_flag = False
+            while len(lasers) > 0 :
+                laser = lasers.pop()
+                # print(laser)
+                cur_pos = laser[0]
+                direction = laser[1]
+                if init_flag:
+                    laser_map_dup[cur_pos[0]][cur_pos[1]] = "#"
+                init_flag = True
+                #char = laser_map[laser[1][0]][laser[1][1]]
+                next_pos = ()
+                match direction:
+                    case "R":
+                        next_pos = (cur_pos[0],cur_pos[1]+1)
+                    case "L":
+                        next_pos = (cur_pos[0],cur_pos[1]-1)
+                    case "U":
+                        next_pos = (cur_pos[0]-1,cur_pos[1])
+                    case "D":
+                        next_pos = (cur_pos[0]+1,cur_pos[1])
+                
+                if next_pos[0] >= 0 and next_pos[0] < len(laser_map) and next_pos[1] >= 0 and next_pos[1] < len(laser_map[0]) and laser not in seen_lasers:
+                    char = laser_map[next_pos[0]][next_pos[1]]
+                    #print("Current Char: ",char, next_pos)
+                    match char:
+                        case ".":
+                            lasers.append((next_pos,direction))
+                        case "|":
+                            if direction == "R" or direction == "L":
+                                if next_pos[0]-1 >= 0:
+                                    lasers.append(((next_pos[0],next_pos[1]), "U"))
+                                if next_pos[0]+1 < len(laser_map):
+                                    lasers.append(((next_pos[0],next_pos[1]), "D"))
+                            else:
+                                lasers.append((next_pos,direction))
+                        case "-":
+                            if direction == "U" or direction == "D":
+                                if next_pos[1]-1 >= 0:
+                                    lasers.append(((next_pos[0],next_pos[1]), "L"))
+                                if next_pos[1]+1 < len(laser_map[0]):
+                                    lasers.append(((next_pos[0],next_pos[1]), "R"))
+                            else:
+                                lasers.append((next_pos,direction))
+                        case "\\":
+                            if direction == "R":
+                                lasers.append(((next_pos[0],next_pos[1]), "D"))
+                            if direction == "L":
+                                #print(":(")
+                                lasers.append(((next_pos[0],next_pos[1]), "U"))
+                            if direction == "U":
+                                lasers.append(((next_pos[0],next_pos[1]), "L"))
+                            if direction == "D":
+                                lasers.append(((next_pos[0],next_pos[1]), "R"))
+                        case "/":
+                            if direction == "R":
+                                lasers.append(((next_pos[0],next_pos[1]), "U"))
+                            if direction == "L":
+                                lasers.append(((next_pos[0],next_pos[1]), "D"))
+                            if direction == "U":
+                                lasers.append(((next_pos[0],next_pos[1]), "R"))
+                            if direction == "D":
+                                lasers.append(((next_pos[0],next_pos[1]), "L"))
+                    #if char == '.':
+                    #    lasers.append((laser[1][1]))
+                    #print(char)
+                    #print("Current Lasers: ",lasers)
+                seen_lasers.add(laser)
+
+                #print_matrix(laser_map_dup)7308 :(
+            #print("")
+            map_string = ""
+            for row in laser_map_dup:
+                map_string += "".join(row)
+            totals.add(map_string.count("#"))
+        # Top Column
+        for i in range(len(laser_map[0])):
+            laser_map_dup =  [x[:] for x in laser_map]
+            lasers = [((-1,i),"D")]
+            seen_lasers = set()
+            init_flag = False
+            while len(lasers) > 0 :
+                laser = lasers.pop()
+                # print(laser)
+                cur_pos = laser[0]
+                direction = laser[1]
+                if init_flag:
+                    laser_map_dup[cur_pos[0]][cur_pos[1]] = "#"
+                init_flag = True
+                #char = laser_map[laser[1][0]][laser[1][1]]
+                next_pos = ()
+                match direction:
+                    case "R":
+                        next_pos = (cur_pos[0],cur_pos[1]+1)
+                    case "L":
+                        next_pos = (cur_pos[0],cur_pos[1]-1)
+                    case "U":
+                        next_pos = (cur_pos[0]-1,cur_pos[1])
+                    case "D":
+                        next_pos = (cur_pos[0]+1,cur_pos[1])
+                
+                if next_pos[0] >= 0 and next_pos[0] < len(laser_map) and next_pos[1] >= 0 and next_pos[1] < len(laser_map[0]) and laser not in seen_lasers:
+                    char = laser_map[next_pos[0]][next_pos[1]]
+                    #print("Current Char: ",char, next_pos)
+                    match char:
+                        case ".":
+                            lasers.append((next_pos,direction))
+                        case "|":
+                            if direction == "R" or direction == "L":
+                                if next_pos[0]-1 >= 0:
+                                    lasers.append(((next_pos[0],next_pos[1]), "U"))
+                                if next_pos[0]+1 < len(laser_map):
+                                    lasers.append(((next_pos[0],next_pos[1]), "D"))
+                            else:
+                                lasers.append((next_pos,direction))
+                        case "-":
+                            if direction == "U" or direction == "D":
+                                if next_pos[1]-1 >= 0:
+                                    lasers.append(((next_pos[0],next_pos[1]), "L"))
+                                if next_pos[1]+1 < len(laser_map[0]):
+                                    lasers.append(((next_pos[0],next_pos[1]), "R"))
+                            else:
+                                lasers.append((next_pos,direction))
+                        case "\\":
+                            if direction == "R":
+                                lasers.append(((next_pos[0],next_pos[1]), "D"))
+                            if direction == "L":
+                                #print(":(")
+                                lasers.append(((next_pos[0],next_pos[1]), "U"))
+                            if direction == "U":
+                                lasers.append(((next_pos[0],next_pos[1]), "L"))
+                            if direction == "D":
+                                lasers.append(((next_pos[0],next_pos[1]), "R"))
+                        case "/":
+                            if direction == "R":
+                                lasers.append(((next_pos[0],next_pos[1]), "U"))
+                            if direction == "L":
+                                lasers.append(((next_pos[0],next_pos[1]), "D"))
+                            if direction == "U":
+                                lasers.append(((next_pos[0],next_pos[1]), "R"))
+                            if direction == "D":
+                                lasers.append(((next_pos[0],next_pos[1]), "L"))
+
+                seen_lasers.add(laser)
+            map_string = ""
+            for row in laser_map_dup:
+                map_string += "".join(row)
+            totals.add(map_string.count("#"))
+        # Right Column
+        for i in range(len(laser_map)):
+            laser_map_dup =  [x[:] for x in laser_map]
+            lasers = [((i,len(laser_map[0])),"L")]
+            seen_lasers = set()
+            init_flag = False
+            while len(lasers) > 0 :
+                laser = lasers.pop()
+                # print(laser)
+                cur_pos = laser[0]
+                direction = laser[1]
+                if init_flag:
+                    laser_map_dup[cur_pos[0]][cur_pos[1]] = "#"
+                init_flag = True
+                #char = laser_map[laser[1][0]][laser[1][1]]
+                next_pos = ()
+                match direction:
+                    case "R":
+                        next_pos = (cur_pos[0],cur_pos[1]+1)
+                    case "L":
+                        next_pos = (cur_pos[0],cur_pos[1]-1)
+                    case "U":
+                        next_pos = (cur_pos[0]-1,cur_pos[1])
+                    case "D":
+                        next_pos = (cur_pos[0]+1,cur_pos[1])
+                
+                if next_pos[0] >= 0 and next_pos[0] < len(laser_map) and next_pos[1] >= 0 and next_pos[1] < len(laser_map[0]) and laser not in seen_lasers:
+                    char = laser_map[next_pos[0]][next_pos[1]]
+                    #print("Current Char: ",char, next_pos)
+                    match char:
+                        case ".":
+                            lasers.append((next_pos,direction))
+                        case "|":
+                            if direction == "R" or direction == "L":
+                                if next_pos[0]-1 >= 0:
+                                    lasers.append(((next_pos[0],next_pos[1]), "U"))
+                                if next_pos[0]+1 < len(laser_map):
+                                    lasers.append(((next_pos[0],next_pos[1]), "D"))
+                            else:
+                                lasers.append((next_pos,direction))
+                        case "-":
+                            if direction == "U" or direction == "D":
+                                if next_pos[1]-1 >= 0:
+                                    lasers.append(((next_pos[0],next_pos[1]), "L"))
+                                if next_pos[1]+1 < len(laser_map[0]):
+                                    lasers.append(((next_pos[0],next_pos[1]), "R"))
+                            else:
+                                lasers.append((next_pos,direction))
+                        case "\\":
+                            if direction == "R":
+                                lasers.append(((next_pos[0],next_pos[1]), "D"))
+                            if direction == "L":
+                                #print(":(")
+                                lasers.append(((next_pos[0],next_pos[1]), "U"))
+                            if direction == "U":
+                                lasers.append(((next_pos[0],next_pos[1]), "L"))
+                            if direction == "D":
+                                lasers.append(((next_pos[0],next_pos[1]), "R"))
+                        case "/":
+                            if direction == "R":
+                                lasers.append(((next_pos[0],next_pos[1]), "U"))
+                            if direction == "L":
+                                lasers.append(((next_pos[0],next_pos[1]), "D"))
+                            if direction == "U":
+                                lasers.append(((next_pos[0],next_pos[1]), "R"))
+                            if direction == "D":
+                                lasers.append(((next_pos[0],next_pos[1]), "L"))
+
+                seen_lasers.add(laser)
+            map_string = ""
+            for row in laser_map_dup:
+                map_string += "".join(row)
+            totals.add(map_string.count("#")) 
+        #Bottom Column
+        for i in range(len(laser_map[0])):
+            laser_map_dup =  [x[:] for x in laser_map]
+            lasers = [((len(laser_map[0]),i),"U")]
+            seen_lasers = set()
+            init_flag = False
+            while len(lasers) > 0 :
+                laser = lasers.pop()
+                # print(laser)
+                cur_pos = laser[0]
+                direction = laser[1]
+                if init_flag:
+                    laser_map_dup[cur_pos[0]][cur_pos[1]] = "#"
+                init_flag = True
+                #char = laser_map[laser[1][0]][laser[1][1]]
+                next_pos = ()
+                match direction:
+                    case "R":
+                        next_pos = (cur_pos[0],cur_pos[1]+1)
+                    case "L":
+                        next_pos = (cur_pos[0],cur_pos[1]-1)
+                    case "U":
+                        next_pos = (cur_pos[0]-1,cur_pos[1])
+                    case "D":
+                        next_pos = (cur_pos[0]+1,cur_pos[1])
+                
+                if next_pos[0] >= 0 and next_pos[0] < len(laser_map) and next_pos[1] >= 0 and next_pos[1] < len(laser_map[0]) and laser not in seen_lasers:
+                    char = laser_map[next_pos[0]][next_pos[1]]
+                    #print("Current Char: ",char, next_pos)
+                    match char:
+                        case ".":
+                            lasers.append((next_pos,direction))
+                        case "|":
+                            if direction == "R" or direction == "L":
+                                if next_pos[0]-1 >= 0:
+                                    lasers.append(((next_pos[0],next_pos[1]), "U"))
+                                if next_pos[0]+1 < len(laser_map):
+                                    lasers.append(((next_pos[0],next_pos[1]), "D"))
+                            else:
+                                lasers.append((next_pos,direction))
+                        case "-":
+                            if direction == "U" or direction == "D":
+                                if next_pos[1]-1 >= 0:
+                                    lasers.append(((next_pos[0],next_pos[1]), "L"))
+                                if next_pos[1]+1 < len(laser_map[0]):
+                                    lasers.append(((next_pos[0],next_pos[1]), "R"))
+                            else:
+                                lasers.append((next_pos,direction))
+                        case "\\":
+                            if direction == "R":
+                                lasers.append(((next_pos[0],next_pos[1]), "D"))
+                            if direction == "L":
+                                #print(":(")
+                                lasers.append(((next_pos[0],next_pos[1]), "U"))
+                            if direction == "U":
+                                lasers.append(((next_pos[0],next_pos[1]), "L"))
+                            if direction == "D":
+                                lasers.append(((next_pos[0],next_pos[1]), "R"))
+                        case "/":
+                            if direction == "R":
+                                lasers.append(((next_pos[0],next_pos[1]), "U"))
+                            if direction == "L":
+                                lasers.append(((next_pos[0],next_pos[1]), "D"))
+                            if direction == "U":
+                                lasers.append(((next_pos[0],next_pos[1]), "R"))
+                            if direction == "D":
+                                lasers.append(((next_pos[0],next_pos[1]), "L"))
+
+                seen_lasers.add(laser)
+            map_string = ""
+            for row in laser_map_dup:
+                map_string += "".join(row)
+            totals.add(map_string.count("#"))
+        
+        print(max(totals))
 
 if __name__ == "__main__":
     input_selection = args.input
