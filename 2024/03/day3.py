7e8341f248a5276bdd1261a29eeffc927814cf8a
Ethan Crann <ecrann@protonmail.com>
2024-12-03 09:27:44 -0330

    cursed addition

--- 2024/03/day3.py
+++ 2024/03/day3.py
@@ -5,6 +5,7 @@ from itertools import pairwise
 import fnmatch
 import re
 import fnmatch
+from numpy import prod
 
 parser = argparse.ArgumentParser()
 parser.add_argument("s", choices=["p1","p2"])
@@ -17,11 +18,15 @@ def part1(filename):
         expressions = re.findall(r"mul\(\d{1,3},\d{1,3}\)",input_string)
         total = 0
         for expression in expressions:
-            print(expression)
             expression = expression.lstrip("mul(").rstrip(")")
             numbers = [int(num) for num in expression.split(",")]
             total += numbers[0]*numbers[1]
         print(total)
+
+def part1_smol(filename):
+    with open(filename) as f:
+        print(sum(list(map(lambda expression: prod([int(num) for num in expression.lstrip("mul(").rstrip(")").split(",")]),re.findall(r"mul\(\d{1,3},\d{1,3}\)",[line for line in f][0])))))
+
 def part2(filename):
     with open(filename) as f:
         input_string = [line for line in f][0];
