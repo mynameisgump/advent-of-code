85e675090f9bf3b785d0f3223d45123407b391fb
Ethan Crann <ecrann@protonmail.com>
2024-12-05 09:33:02 -0330

    Clean up

--- 2024/05/day5.py
+++ 2024/05/day5.py
@@ -3,74 +3,58 @@ import re
 from functools import cmp_to_key
 
 parser = argparse.ArgumentParser()
-parser.add_argument("solution", choices=["p1","p2"])
-parser.add_argument("input", choices=["i1","ex1","ex2"])
-args = parser.parse_args();
+parser.add_argument("solution", choices=["p1", "p2"])
+parser.add_argument("input", choices=["i1", "ex1", "ex2"])
+args = parser.parse_args()
+
 
 def part1(filename):
     with open(filename) as f:
-        lines = [l.split("\n") for l in f.read().split("\n\n")];
+        lines = [l.split("\n") for l in f.read().split("\n\n")]
         rules = [line.split("|") for line in lines[0]]
         updates = [line.split(",") for line in lines[1]]
         rulebook = {}
-        reverse_rulebook = {}
         for rule in rules:
             if rule[0] in rulebook:
                 rulebook[rule[0]].append(rule[1])
             else:
                 rulebook[rule[0]] = [rule[1]]
-            if rule[1] in reverse_rulebook:
-                reverse_rulebook[rule[1]].append(rule[0])
-            else:
-                reverse_rulebook[rule[1]] = [rule[0]]
-        print(rulebook)
+
         total = 0
         for update in updates:
-            print()
-            print(update)
             valid = True
             for index in range(len(update)):
                 value = update[index]
-                print("Current Val:", value)
-                before = set(update[:index])
-                after = set(update[index+1:])
+                after = set(update[index + 1 :])
                 rule = []
                 if value in rulebook:
                     rule = set(rulebook[value])
-                
-                print("Before Rules: ",rule)
+
                 if not after.issubset(rule):
                     valid = False
 
             if valid:
-                middle_num = int(update[len(update)//2])
+                middle_num = int(update[len(update) // 2])
                 total += middle_num
 
-        print(total)
-                #Step 1: Check list forwards for 
-          
+        print("Total: ", total)
 
 
 def part2(filename):
     with open(filename) as f:
-        lines = [l.split("\n") for l in f.read().split("\n\n")];
+        lines = [l.split("\n") for l in f.read().split("\n\n")]
         rules = [line.split("|") for line in lines[0]]
         updates = [line.split(",") for line in lines[1]]
         rulebook = {}
-        reverse_rulebook = {}
         for rule in rules:
             if rule[0] in rulebook:
                 rulebook[rule[0]].append(rule[1])
             else:
                 rulebook[rule[0]] = [rule[1]]
-            if rule[1] in reverse_rulebook:
-                reverse_rulebook[rule[1]].append(rule[0])
-            else:
-                reverse_rulebook[rule[1]] = [rule[0]]
 
-        def sort_algo(a,b):
+        def sort_algo(a, b):
             if a in rulebook:
-                rule = rulebook[a];
+                rule = rulebook[a]
                 if b in rule:
                     return -1
                 else:
@@ -80,40 +64,34 @@ def part2(filename):
 
         total = 0
         for update in updates:
-            
             valid = True
             for index in range(len(update)):
                 value = update[index]
-                before = set(update[:index])
-                after = set(update[index+1:])
+                after = set(update[index + 1 :])
                 rule = []
                 if value in rulebook:
                     rule = set(rulebook[value])
-                
+
                 if not after.issubset(rule):
                     valid = False
 
             if valid == False:
-                print(update)
-                rest = sorted(update,key=cmp_to_key(sort_algo))
-                print(rest)
-                middle_num = int(rest[len(rest)//2])
+                sorted_update = sorted(update, key=cmp_to_key(sort_algo))
+                middle_num = int(sorted_update[len(sorted_update) // 2])
                 total += middle_num
-                print("Magic Time")
 
-        print(total)
-                #Step 1: Check list forwards for 
-          
+        print("Total: ", total)
+
 
 if __name__ == "__main__":
     input_selection = args.input
-    solution_selection = args.solution;
+    solution_selection = args.solution
     filename = ""
     match input_selection:
         case "i1":
-            filename="input.txt"
+            filename = "input.txt"
         case "ex1":
-            filename="ex1.txt"
+            filename = "ex1.txt"
     match solution_selection:
         case "p1":
             part1(filename)
