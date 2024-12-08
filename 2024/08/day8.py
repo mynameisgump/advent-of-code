e4f0b890810ef4d69a48952cf83ad6b2738c7e69
Ethan Crann <ecrann@protonmail.com>
2024-12-08 09:11:38 -0330

    Day 2 complete

--- 2024/08/day8.py
+++ 2024/08/day8.py
@@ -38,7 +38,48 @@ def part1(filename):
 
 def part2(filename):
     with open(filename) as f:
-        lines = f.read().split("\n");
+        file_in = f.read().split("\n")
+        lines = [list(line) for line in file_in];
+
+        antinode_positions = set()
+        frequencies = {}
+        for x in range(len(lines)):
+            for y in range(len(lines[x])):
+                char = lines[x][y]
+                if char != ".":
+                    if char not in frequencies:
+                        frequencies[char] = [(x,y)]
+                    else:
+                        frequencies[char].append((x,y))
+        
+        for key in frequencies:
+            positions = frequencies[key]
+            pairs = list(itertools.combinations(positions,2))
+            for pair in pairs:
+                x_dist = pair[0][0]-pair[1][0]
+                y_dist = pair[0][1]-pair[1][1]
+                
+                antinode_1 = (pair[0][0]+x_dist,pair[0][1]+y_dist)
+                antinode_2 = (pair[1][0]-x_dist,pair[1][1]-y_dist)
+                while 0 <= antinode_1[0] < len(lines) and 0 <= antinode_1[1] < len(lines[0]):
+                    antinode_positions.add(antinode_1)
+                    antinode_1 = (antinode_1[0]+x_dist,antinode_1[1]+y_dist)
+                
+                while 0 <= antinode_2[0] < len(lines) and 0 <= antinode_2[1] < len(lines[0]):
+                    antinode_positions.add(antinode_2)
+                    antinode_2 = (antinode_2[0]-x_dist,antinode_2[1]-y_dist)
+                    print(antinode_2)
+                antinode_positions.add(pair[0])
+                antinode_positions.add(pair[1])
+                # antinode_positions.add((pair[0][0]+x_dist,pair[0][1]+y_dist))
+                # antinode_positions.add()
+
+
+        for antinode in antinode_positions:
+            lines[antinode[0]][antinode[1]] = "#"
+        for line in lines:
+            print("".join(line))
+        print(len(antinode_positions))
 
 if __name__ == "__main__":
     input_selection = args.input
