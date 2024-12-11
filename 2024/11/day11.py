8efa89fd3498d9629dc035d04d5dcb1e32dc59ae
Ethan Crann <ecrann@protonmail.com>
2024-12-11 10:01:44 -0330

    Will try multreading on the chunks :(

--- 2024/11/day11.py
+++ 2024/11/day11.py
@@ -1,5 +1,6 @@
 import argparse
 import re
+from collections import deque
 parser = argparse.ArgumentParser()
 parser.add_argument("solution", choices=["p1","p2"])
 parser.add_argument("input", choices=["i1","ex1","ex2"])
@@ -19,7 +20,7 @@ def part1(filename):
                     lines[current_index] = str(1)
                 elif len(stone) % 2 == 0:
                     new_stones = [str(int(stone[:len(stone)//2])), str(int(stone[len(stone)//2:]))]
-                    lines = lines[:current_index]+new_stones+lines[current_index+1:]
+                    lines[current_index:current_index+1] = new_stones 
                     current_index += 1
                 else:
                     lines[current_index] = str(int(lines[current_index])*2024)
@@ -31,7 +32,43 @@ def part1(filename):
 
 def part2(filename):
     with open(filename) as f:
-        lines = f.read().split("\n");
+        blinks = 75
+        current_index = 0
+
+        stone_chunks = [f.read().split("\n")[0].split(" ")];
+        chunk_size = 5000
+        for i in range(blinks):
+            print(f'Blinks: {i}/{blinks}')
+            new_chunks = []
+            for stone_chunk in stone_chunks:
+                while current_index < len(stone_chunk):
+                    stone = stone_chunk[current_index]
+                    if int(stone) == 0:
+                        stone_chunk[current_index] = str(1)
+                    elif len(stone) % 2 == 0:
+                        new_stones = [str(int(stone[:len(stone)//2])), str(int(stone[len(stone)//2:]))]
+                        stone_chunk[current_index:current_index+1] = new_stones 
+                        current_index += 1
+                    else:
+                        stone_chunk[current_index] = str(int(stone_chunk[current_index])*2024)
+                        
+                    current_index += 1
+                    
+                if len(stone_chunk) > chunk_size:
+                    new_chunks.append(stone_chunk[:len(stone_chunk)//2])
+                    new_chunks.append(stone_chunk[len(stone_chunk)//2:])
+                else:
+                    new_chunks.append(stone_chunk)
+                current_index = 0
+
+            stone_chunks = new_chunks
+            
+        final_total = 0
+        for chunk in stone_chunks:
+            final_total += len(chunk)
+        print(final_total)
+
+
 
 if __name__ == "__main__":
     input_selection = args.input
