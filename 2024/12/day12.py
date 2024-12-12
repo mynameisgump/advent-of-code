4ab3e37138137740b798a22c66f5445e46d10d59
Ethan Crann <ecrann@protonmail.com>
2024-12-12 10:06:22 -0330

    p1

--- 2024/12/day12.py
+++ 2024/12/day12.py
@@ -17,6 +17,17 @@ def get_neighbours(position,max_h,max_w):
     if position[1]+1 < max_h:
        neighbours.append((position[0],position[1]+1))    
     return neighbours
+def get_diagonal_neighbours(position,max_h,max_w):
+    neighbours = []
+    if position[1]-1 >= 0 and position[0]-1 >= 0:
+        neighbours.append((position[0]-1,position[1]-1))
+    if position[0]-1 >= 0 and position[1]+1 < max_w:
+        neighbours.append((position[0]-1,position[1]+1))
+    if position[0]+1 < max_h and position[1]-1 >= 0:
+        neighbours.append((position[0]+1,position[1]-1))
+    if position[0]+1 < max_h and position[1]+1 < max_w:
+        neighbours.append((position[0]+1,position[1]+1))
+    return neighbours
 
 def part1(filename):
     with open(filename) as f:
@@ -25,28 +36,43 @@ def part1(filename):
         max_w = len(garden)
         selected = None
         visited = set()
+        total = 0 
         for x in range(len(garden)):
             for y in range(len(garden[x])):
                 if (x,y) in visited:
                     pass
                 else:
+                    print()
                     value = garden[x][y]
                     searching = True
                     to_search = [(x,y)]
                     group = set()
+                    peri = 0
+                    area = 0
+                    # print(to_search)
                     while searching:
                         if len(to_search) == 0:
                             searching = False
                             break
+                        
                         current_item = to_search.pop()
+                        # print(current_item)
+                        area += 1
+
                         group.add(current_item)
                         neighbours = get_neighbours(current_item,max_h,max_w)
+                        valid_neighbours = 0
                         for n in neighbours:
-                            if garden[n[0]][n[1]] == value and (n[0],n[1]) not in visited:
-                                to_search.append(n)
+                            if garden[n[0]][n[1]] == value:
+                                valid_neighbours += 1
+                                if (n[0],n[1]) not in visited and (n[0],n[1]) not in to_search:
+                                    to_search.append(n)
+                        peri += 4-valid_neighbours
                         visited.add((current_item[0],current_item[1])) 
-                    # print("Visited:", visited)
-                    print("Group: ", group)
+
+                    total += area*peri
+
+        print(total)
                     
 
 
